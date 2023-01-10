6ef77578ac15d62daafc76ff5ecd2f96e847699f
Gustavo Valente <gustavo.pw00@gmail.com>
2023-01-10 19:41:45 -0300

    Add main.c and src folder

---
+++ src/main.c
@@ -0,0 +1,10 @@
+/**********HEADER************
+ * Gustavo Valente - GRR
+ * Bruno Farias - GRR
+ *****************************/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
